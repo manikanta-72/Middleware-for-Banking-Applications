ea2970fcf7cf374ec314cd3847c73b91ec490f2c
hariuserx <harispartacus0@gmail.com>
2022-05-28 17:52:27 -0700

    Add templates

---
+++ agent/agent.py
@@ -0,0 +1,9 @@
+# Handle Read
+def read(account_numbers):
+    # return a dict of account number and balance along with current timestamp
+    pass
+
+
+# Handle COMMIT
+def commit(read_account_numbers, write_account_numbers, read_timestamp, transaction_sno):
+    pass
